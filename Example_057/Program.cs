73b28bd36db0635b92d03dd9c7684935f0fc7c6f
wargie <wargie@bk.ru>
2022-02-28 15:48:38 +0200

    Отладка кода к задаче №57

--- Example_057/Program.cs
+++ Example_057/Program.cs
@@ -1,57 +1,59 @@
 ﻿// Задача 57: Написать программу, которая упорядочивает по убыванию элементы каждой строки двумерной массива.
 
-int row = 4, col = 7,  min = 10,  max = 100;
+int row = 4, col = 7;
+Random random = new Random();
 
-int[,] GetMatrix(int row, int col, int min, int max)
+int[,] GetMatrix(int row, int col)
     {
-        int[,] matrix = new int[row, col];
-            for (int i = 0; i < matrix.GetLength(0); i++)
+        int[,] arr = new int[row, col];
+
+            for (int i = 0; i < arr.GetLength(0); i++)
                 {
-                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    for (int j = 0; j < arr.GetLength(1); j++)
                         {
-                            matrix[i, j] = new Random().Next(min, max);
+                            arr[i, j] =  random.Next(1, 15);
                         }
                 }
-            return matrix;
+            return arr;
     }
 
 int[,] SortRowAscending(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            int minPosition = j;                                            
-            for (int k = j + 1; k < array.GetLength(1); k++)                
+        for (int i = 0; i < array.GetLength(0); i++)
             {
-                if (array[i, k] > array[i, minPosition]) minPosition = k;    
+                for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        int minPosition = j;                                            
+                        for (int k = j + 1; k < array.GetLength(1); k++)                
+                            {
+                                if (array[i, k] > array[i, minPosition]) minPosition = k;    
+                            }
+                        int tmp = array[i, j];                                          
+                        array[i, j] = array[i, minPosition];                            
+                        array[i, minPosition] = tmp;                                    
+                    }
+
             }
-            int tmp = array[i, j];                                          
-            array[i, j] = array[i, minPosition];                            
-            array[i, minPosition] = tmp;                                    
-        }
-
+        return array;
     }
-    return array;
-}
 
-void PrintTable(int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
+void PrintTable(int[,] arr)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            Console.Write(matrix[i, j] + "  ");
-        }
-        Console.WriteLine();
+        for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                    {
+                        Console.Write(arr[i, j] + "  ");
+                    }
+                Console.WriteLine();
+            }
     }
-}
 
 
 
-int[,] table = GetMatrix(row, col, min, max);
+int[,] table = GetMatrix(row, col);
 PrintTable(table);
 
-System.Console.WriteLine();
+Console.WriteLine();
 int[,] rowSort = SortRowAscending(table);
 PrintTable(rowSort);
