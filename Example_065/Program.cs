f421019082662d5a621fdcb8431b05abeffb1dc0
wargie <wargie@bk.ru>
2022-03-01 09:13:53 +0200

    Начато решение задачи №65

---
+++ Example_065/Program.cs
@@ -0,0 +1,47 @@
+﻿//Задача 65: Спирально заполнить двумерный массив
+
+void FillArray2D(int[,] array, int minNumber = 0, int maxNumber = 9)
+{
+    Random rnd = new Random();
+    int numberRectangle = 0;
+
+    int M = array.GetLength(0), N = array.GetLength(1), i = 0, j = 0, number = 1;
+
+    while (number <= M * N)
+    {
+        array[i, j] = number;
+
+        if (i == numberRectangle && j < N - numberRectangle - 1) j++;
+        else if (j == N - numberRectangle - 1 && i < M - numberRectangle - 1) i++;
+        else if (i == M - numberRectangle - 1 && j > numberRectangle) j--;
+        else i--;
+
+        if (i == numberRectangle + 1 && j == numberRectangle && numberRectangle != N - numberRectangle - 1) 
+        
+        {
+            numberRectangle++;
+        }   
+        number++;
+
+    }
+
+}
+
+void PrintArray2D(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == array.GetLength(1) - 1) Console.Write($"{array[i, j]}.");
+            else Console.Write($"{array[i, j]}, ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] array = new int[6, 6];
+FillArray2D(array);
+PrintArray2D(array);
