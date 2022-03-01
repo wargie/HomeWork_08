4fbfa7afe6eb99012fc36db5a310f133faf97fb2
wargie <wargie@bk.ru>
2022-03-01 09:04:13 +0200

    Поиск ошибки в коде к задаче №63

--- Example_063/Program.cs
+++ Example_063/Program.cs
@@ -1,10 +1,11 @@
-﻿//Задача 63: Сформировать трехмерный массив не повторяющимися двузначными числами показать его построчно на экран выводя индексы соответствующего элемента
+﻿//Задача 63: Сформировать трехмерный массив не повторяющимися двузначными числами. Показать его построчно, выводя на экран индексы соответствующего элемента.
 
-int[,,] array = new int[4, 4,4];
+int[,,] array = new int[4, 4, 4];
 
 void FillArray3D(int[,,] array)
 {
-    int number = 21;
+    int number = 10;
+
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
@@ -27,7 +28,7 @@ void PrintArray3D(int[,,] array)
         {
             for (int k = 0; k < array.GetLength(2); k++)
             {
-                Console.WriteLine($"Элемент с индексом [{i}, {j}, {k}] - {array[i, j, k]}.");
+                Console.WriteLine($"Элемент с индексом [{i}, {j}, {k}] - {array[i, j, k]} ");
 
             }
             Console.WriteLine();
